8a8d0ba405aa673864662a6be889a180d725f2c1
Paul Saada <paulsaada6@gmail.com>
2019-11-24 07:51:50 -0500

    built email form

--- src/containers/EmailForm.jsx
+++ src/containers/EmailForm.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import EmailField from '../components/EmailField';
+import SubmitButton from '../components/SubmitButton';
+import '../stylesheets/EmailForm.css';
 
 class EmailForm extends Component {
    state = {}
    render() {
-      return (  );
+      return (
+         <div>
+            <EmailField />
+            <SubmitButton />
+         </div>
+      );
    }
 }
 
