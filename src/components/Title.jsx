bf5f6299226122a36d0f51979ddff7c89546fc9c
Paul Saada <paulsaada6@gmail.com>
2019-11-24 06:30:41 -0500

    added title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Title extends Component {
+   state = {}
+   render() {
+      return (
+         <div>
+            <p className="title">join the list</p>
+         </div>
+      );
+   }
+}
+
+export default Title;
