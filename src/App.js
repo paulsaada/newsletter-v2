abcc4263bcb0e4402045555365f89810b3405d12
Paul Saada <paulsaada6@gmail.com>
2019-11-24 07:06:38 -0500

    added field to app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import SubmitButton from './components/SubmitButton';
 import Title from './components/Title';
+import Subtitle from './components/Subtitle';
+import EmailField from './components/EmailField';
 import './stylesheets/App.css';
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <Subtitle />
+      <EmailField />
       <SubmitButton />
     </div>
   );
