dd7122dd0bb8bec89d21b52a34b77254d37d9138
Paul Saada <paulsaada6@gmail.com>
2019-11-24 06:36:01 -0500

    added title styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import SubmitButton from './components/SubmitButton'
+import SubmitButton from './components/SubmitButton';
+import Title from './components/Title';
 import './stylesheets/App.css';
 
 function App() {
   return (
     <div className="App">
+      <Title />
       <SubmitButton />
     </div>
   );
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../stylesheets/Title.css'
 
 class Title extends Component {
    state = {}
