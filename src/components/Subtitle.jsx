9df64a8633743c171b300cd0bf0c64495832aedf
Paul Saada <paulsaada6@gmail.com>
2019-11-24 07:05:31 -0500

    added subtitle component

---
+++ src/components/Subtitle.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../stylesheets/Subtitle.css'
+
+class Subtitle extends Component {
+   state = {}
+   render() {
+      return (
+         <div>
+            <p className="subtitle">SIGN UP FOR THE TLC NEWSLETTER</p>
+         </div>
+      );
+   }
+}
+
+export default Subtitle;
