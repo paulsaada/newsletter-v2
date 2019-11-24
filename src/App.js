3719eec25562046c9224bc43136bf85e65b30d31
Paul Saada <paulsaada6@gmail.com>
2019-11-24 07:24:43 -0500

    added name field component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import SubmitButton from './components/SubmitButton';
 import Title from './components/Title';
 import Subtitle from './components/Subtitle';
 import EmailField from './components/EmailField';
+import NameField from './components/NameField';
+import Checkbox from './components/Checkbox';
 import './stylesheets/App.css';
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <Title />
       <Subtitle />
       <EmailField />
+      <NameField />
+      <Checkbox />
       <SubmitButton />
     </div>
   );
