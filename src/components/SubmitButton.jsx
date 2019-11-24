49e544032db9a88bff15a7ccb2b9129d2fc011d7
Paul Saada <paulsaada6@gmail.com>
2019-11-24 05:58:40 -0500

    initialized folder structure

---
+++ src/components/SubmitButton.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SubmitButton extends Component {
+   state = {}
+   render() {
+      return (
+         <div>
+            <button>NEXT</button>
+         </div>
+      );
+   }
+}
+
+export default SubmitButton;
